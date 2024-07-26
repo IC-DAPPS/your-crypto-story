e3b8740cafb20124d7f8439666400d6d127e8d00
vishnu vishwakarma <vv083150@gmail.com>
2024-07-26 16:41:12 +0530

    navbar

---
+++ src/frontend/src/routes/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Avatar,
+		Dropdown,
+		DropdownItem,
+		DropdownHeader,
+		DropdownDivider
+	} from 'flowbite-svelte';
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<Navbar>
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span
+		>
+	</NavBrand>
+	<div class="flex items-center md:order-2">
+		<Avatar id="avatar-menu">Vi</Avatar>
+		<NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
+	</div>
+	<Dropdown placement="bottom" triggeredBy="#avatar-menu">
+		<DropdownHeader>
+			<span class="block text-sm">Bonnie Green</span>
+			<span class="block truncate text-sm font-medium">name@flowbite.com</span>
+		</DropdownHeader>
+		<DropdownItem>Dashboard</DropdownItem>
+		<DropdownItem>Settings</DropdownItem>
+		<DropdownItem>Earnings</DropdownItem>
+		<DropdownDivider />
+		<DropdownItem>Sign out</DropdownItem>
+	</Dropdown>
+	<NavUl>
+		<NavLi href="/" active={true}>Home</NavLi>
+		<NavLi href="/about">About</NavLi>
+		<NavLi href="/docs/components/navbar">Navbar</NavLi>
+		<NavLi href="/pricing">Pricing</NavLi>
+		<NavLi href="/contact">Contact</NavLi>
+	</NavUl>
+</Navbar>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.500);
+	}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/frontend/src//*.{html,js,svelte,ts}',
-    './node_modules/flowbite-svelte//*.{html,js,svelte,ts}'],
+  darkMode: 'class',
+  content: [
+      './src/frontend/src/**/*.{html,js,svelte,ts}',
+      './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+      extend: {
+          colors: {
+              // flowbite-svelte
+              primary: {
+                  50: '#f9f9f9',
+                  100: '#f2f2f2',
+                  200: '#e5e5e5',
+                  300: '#cccccc',
+                  400: '#999999',
+                  500: '#666666',
+                  600: '#4d4d4d',
+                  700: '#333333',
+                  800: '#1a1a1a',
+                  900: '#000000'
+              }
+          }
+      }
+  }
+  // plugins: [require('flowbite/plugin')]
+};
