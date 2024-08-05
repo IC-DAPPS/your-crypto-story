822488795107fadd3ec5faf08007e7b5f2fc7083
Rahees <ahdrahees.dev@gmail.com>
2024-08-05 11:41:26 +0530

    chore: Ignore .zed files, update deploy-local.sh

--- scripts/deploy-local.sh
+++ scripts/deploy-local.sh
@@ -11,23 +11,23 @@ dfx deps pull
 dfx deps init --argument '(null)' internet-identity
 dfx deps deploy
 
-# Deploy Backend Canister
-echo "Deploying Backend Canister..."
-dfx deploy backend
- sleep 15
-
 # Run Cargo tests to generate Candid file
 echo "Running Cargo tests to generate Candid file..."
 cargo test
 sleep 5
+# Deploy Backend Canister
+echo "Deploying Backend Canister..."
+dfx deploy backend
+sleep 15
+
 # Generate declarations
 echo "Generating declarations..."
 dfx generate backend
 
 # deploy frontend
-echo "Deploying frontend..."
-npm install
-dfx deploy frontend
+# echo "Deploying frontend..."
+# npm install
+# dfx deploy frontend
 
 # Inform the user that the deployment completed successfully
 echo "Deployment completed successfully. Now you can start your local development server by running 'npm run dev'"
