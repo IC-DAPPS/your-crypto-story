8937eb065ba47895a62896f2c3ded9bd868b217b
Rahees <ahdrahees.dev@gmail.com>
2024-09-16 22:28:32 +0530

    fix: Modified Navbar into new UI

--- src/frontend/src/lib/components/NavBar.svelte
+++ src/frontend/src/lib/components/NavBar.svelte
@@ -1,49 +1,62 @@
-<script>
-	import Navbar from 'flowbite-svelte/Navbar.svelte';
-	import NavBrand from 'flowbite-svelte/NavBrand.svelte';
-	import NavLi from 'flowbite-svelte/NavLi.svelte';
-	import NavUl from 'flowbite-svelte/NavUl.svelte';
-	import NavHamburger from 'flowbite-svelte/NavHamburger.svelte';
-	import Button from 'flowbite-svelte/Button.svelte';
+<script lang="ts">
+	import Avatar from '@/components/Avatar.svelte';
+	import ButtonWithSpinner from '@/components/ButtonWithSpinner.svelte';
+	import { authStore } from '@/stores/auth.store';
 	import { page } from '$app/stores';
-	import { authStore } from '$lib/stores/auth.store';
-	import UserMenu from '$lib/components/UserMenu.svelte';
-	import DarkMode from 'flowbite-svelte/DarkMode.svelte';
-	import DarkModeButton from '$lib/components/DarkModeButton.svelte';
-	import { userStore } from '$lib/stores/user.store';
+	import LightAndDarkToggle from '@/components/LightAndDarkToggle.svelte';
 
-	$: activeUrl = $page.url.pathname;
+	const Pages = [
+		{ name: 'Home', link: '/' },
+		{ name: 'Transactions', link: '/transactions/' },
+		{ name: 'Portfolio', link: '/portfolio/' },
+		{ name: 'Story', link: '/story/' }
+	];
 </script>
 
-<Navbar class="border-b border-primary-400">
-	<NavBrand href={$userStore.isRegistered ? '/' : ''}>
-		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-			>Your Crypto Story</span
-		>
-	</NavBrand>
-	<div class="flex items-center md:order-2">
-		<DarkModeButton btnClass="hidden md:block p-2 mr-3" />
-		{#if $authStore.isAuthenticated && $userStore.isRegistered}
-			<UserMenu />
-		{:else if $authStore.isAuthenticated && !$userStore.isRegistered}
-			<Button color="alternative" on:click={authStore.signOut}>Log out</Button>
-		{:else}
-			<Button on:click={authStore.signIn}>Log in</Button>
-		{/if}
-		<NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
-	</div>
+<nav class=" px-8 py-4">
+	<div>
+		<div class="flex items-center justify-between">
+			<div class="text-2xl font-bold"><a href="/">Y C S</a></div>
 
-	<NavUl {activeUrl}>
-		{#if $userStore.isRegistered}
-			<NavLi href="/" active={true}>Home</NavLi>
-			<NavLi href="/transactions/">Transactions</NavLi>
-			<NavLi href="/portfolio/">Portfolio</NavLi>
-			<NavLi href="/story/">Your story</NavLi>
-		{:else if !$userStore.isRegistered && $authStore.isAuthenticated}
-			<NavLi href="/register/">Register</NavLi>
-		{/if}
-		<DarkMode
-			btnClass="hidden max-md:block ml-3 rounded-full p-2 px-6 w-fit border-2 border-primary-200 text-primary-500 dark:border-white dark:text-white"
-		/>
-	</NavUl>
-</Navbar>
+			<ul class="hidden list-none gap-8 md:flex">
+				{#each Pages as { name, link }, index (index)}
+					<li>
+						<a
+							href={link}
+							class="transition-colors duration-200 hover:text-accent-foreground {$page.url
+								.pathname === link
+								? 'font-bold text-primary'
+								: 'font-light text-muted-foreground'}">{name}</a
+						>
+					</li>
+				{/each}
+			</ul>
+
+			{#if $authStore.isAuthenticated}
+				<Avatar />
+			{:else}
+				<div class="flex items-center gap-4">
+					<LightAndDarkToggle />
+					<ButtonWithSpinner onClick={authStore.signIn}>Login</ButtonWithSpinner>
+				</div>
+			{/if}
+		</div>
+		<div class="overflow-x-auto md:hidden">
+			<ul
+				class="flex list-none justify-center gap-8 whitespace-nowrap pt-4 max-[396px]:justify-start"
+			>
+				{#each Pages as { name, link }, index (index)}
+					<li>
+						<a
+							href={link}
+							class="transition-colors duration-200 hover:text-accent-foreground {$page.url
+								.pathname === link
+								? 'font-bold text-primary'
+								: 'font-light text-muted-foreground'}">{name}</a
+						>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
+</nav>
