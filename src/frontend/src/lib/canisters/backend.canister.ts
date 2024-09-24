317fd4924520e3cb0e4893303587d49abf6f6fac
Rahees <ahdrahees.dev@gmail.com>
2024-09-24 23:23:24 +0530

    add: Backend Canister

---
+++ src/frontend/src/lib/canisters/backend.canister.ts
@@ -0,0 +1,80 @@
+import { getAgent } from '$lib/actors/agents.ic';
+import type { InsertUserDataParams, UpdatePrincipalNameParams } from '$lib/types/api';
+import type { CreateCanisterOptions } from '$lib/types/canister';
+import {
+	idlFactory as idlFactoryBackend,
+	type PrincipalName,
+	type _SERVICE as BackendService,
+	type Result,
+	type Result_1,
+	type Result_2
+} from '@declarations/backend/backend.did';
+import { Canister, createServices, type QueryParams } from '@dfinity/utils';
+
+export class BackendCanister extends Canister<BackendService> {
+	static async create({ identity, ...options }: CreateCanisterOptions<BackendService>) {
+		const agent = await getAgent({ identity });
+
+		const { service, certifiedService, canisterId } = createServices<BackendService>({
+			options: { agent, ...options },
+			idlFactory: idlFactoryBackend,
+			certifiedIdlFactory: idlFactoryBackend
+		});
+
+		return new BackendCanister(canisterId, service, certifiedService);
+	}
+
+	deleteKnownPrincipal = async ({ index }: { index: bigint }): Promise<Result> => {
+		const { delete_known_principal } = this.caller({ certified: true });
+
+		return delete_known_principal(index);
+	};
+
+	deleteOwnedPrincipal = async ({ index }: { index: bigint }): Promise<Result> => {
+		const { delete_owned_principal } = this.caller({ certified: true });
+
+		return delete_owned_principal(index);
+	};
+
+	getUserdata = async ({ certified }: QueryParams): Promise<Result_1> => {
+		const { get_userdata } = this.caller({ certified });
+
+		return get_userdata();
+	};
+
+	insertKnownPrincipals = async (principalAndName: Array<PrincipalName>): Promise<Result_2> => {
+		const { insert_known_principals } = this.caller({ certified: true });
+
+		return insert_known_principals(principalAndName);
+	};
+
+	insertOwnedPrincipals = async (principalAndName: Array<PrincipalName>): Promise<Result_2> => {
+		const { insert_owned_principals } = this.caller({ certified: true });
+
+		return insert_owned_principals(principalAndName);
+	};
+
+	insertUserdata = async ({ name, email }: InsertUserDataParams): Promise<Result_2> => {
+		const { insert_userdata } = this.caller({ certified: true });
+
+		return insert_userdata(name, email);
+	};
+
+	updateKnownPrincipalName = async ({
+		name,
+		index
+	}: UpdatePrincipalNameParams): Promise<Result> => {
+		const { update_known_principal_name } = this.caller({ certified: true });
+
+		return update_known_principal_name(name, index);
+	};
+
+	updateOwnedPrincipalName = async ({
+		name,
+		index
+	}: UpdatePrincipalNameParams): Promise<Result> => {
+		const { update_owned_principal_name } = this.caller({ certified: true });
+
+		return update_owned_principal_name(name, index);
+	};
+}
